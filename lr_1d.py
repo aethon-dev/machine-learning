9643c8f2d2dbdae290fd330405673adc791eb3e7
Kasun Perera <kape@mazarin.lk>
2016-11-30 16:54:32 +0530

    initial commit

---
+++ lr_1d.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plot
+
+# load the data
+X = []
+Y = []
+for line in open("data_1d.csv"):
+    x, y = line.split(",")
+    X.append(float(x))
+    Y.append(float(y))
+
+# convert X and Y into numpy arrays
+X = np.array(X)
+Y = np.array(Y)
+
+# plot to see what it looks like
+plot.scatter(X, Y)
+plot.show()
+
+# apply the equations we learned to calculate a and b
+denominator = X.dot(X) - X.mean()*X.sum()
+a = ( X.dot(Y) - Y.mean()*X.sum() ) / denominator
+b = ( Y.mean()*X.dot(X) - X.mean()*X.dot(Y) ) / denominator
+
+Y_hat = a*X + b
+
+# plot it
+plot.scatter(X, Y)
+plot.plot(X, Y_hat)
+plot.show()
